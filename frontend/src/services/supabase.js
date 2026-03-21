430b741a6f6e724cff08a60fcbee3a7148210326
Roberto Biagetti <rbiagetti2001@gmail.com>
2026-03-21 15:17:56 +0100

    fix: supabase client with JWT token

--- frontend/src/services/supabase.js
+++ frontend/src/services/supabase.js
@@ -3,4 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  global: {
+    headers: {
+      get Authorization() {
+        const token = localStorage.getItem('token')
+        return token ? `Bearer ${token}` : ''
+      }
+    }
+  }
+})
